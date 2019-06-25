88ab489e25641838118d3c64d80ef46c65031f49
Ivan Siderov <ivan.siderov@accedia.com>
2019-06-25 13:20:20 +0300

    fixed jest test

--- src/components/organisms/Header/Header.feature
+++ src/components/organisms/Header/Header.feature
@@ -2,6 +2,4 @@ Feature: Header
 
   Scenario: Showing the Header
     Given I have access to the application
-    When I open the application on a any page
-    Then the Header should display on top of the page
-    And the Links specified in the config should be in it
+    Then the Links specified in the config should be in it
--- src/components/organisms/Header/Header.steps.js
+++ src/components/organisms/Header/Header.steps.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+
 import { defineFeature, loadFeature } from 'jest-cucumber'
 import TestRenderer from 'react-test-renderer'
 
 import Header from './Header'
-
+import { headerLinks } from 'utils/config'
 
 const featureFileName = 'Header.feature'
 const featurePath = require('path').resolve(__dirname, featureFileName)
@@ -11,20 +13,18 @@ const feature = loadFeature(featurePath)
 
 defineFeature(feature, test => {
   test('Showing the Header', ({ given, when, then }) => {
+    let component;
     given('I have access to the application', () => {
-      const component = TestRenderer.create(<Header />)
-      console.log('header json ', compoennt.toJSON())
-    })
-
-    when('I open the application on a any page', () => {
-    })
-
-    then('the Header should display on top of the page', () => {
-      expect(true).toBe(true)
+      component = TestRenderer.create(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      )
     })
 
     then('the Links specified in the config should be in it', () => {
-      expect(false).toBe(false)
+      const aContainer = component.toJSON().children[1]
+      expect(aContainer.children.length === headerLinks.length).toBe(true)
     })
   })
 })
